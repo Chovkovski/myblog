c8de9ca1330e0c6ea374b67eba6260651a868e9e
张夏挺 <Pavel@zhangxiatingdeMacBook-Air.local>
2014-07-31 13:47:55 +0800

    basic modal

--- static/js/common.js
+++ static/js/common.js
@@ -3,11 +3,14 @@ function replace_tag(tag, tag_name) {
 }
 
 $(document).ready(function() {
+	$("audio").audioPlayer();
+
 	$(".tags").hover(
 		function() {
 			replace_tag(this, $(this).attr("id") + "_checked.png");
-			var tag_id = "#" + $(this).attr("id") + "_nav";
-			$(tag_id).css("visibility", "visible");
+			var tag_nav = "#" + $(this).attr("id") + "_nav";
+			$(".nav").css("z-index", "50");
+			$(tag_nav).css("visibility", "visible");
 		},
 		function() {
 			replace_tag(this, $(this).attr("id") + ".png");
@@ -16,10 +19,32 @@ $(document).ready(function() {
 
 	$(".nav").mouseleave(function() {
 		$(this).find("ul").css("visibility", "hidden");
+		$(".nav").css("z-index", "-2");
 	});
 
-	$("li").click(function() {
+	$(".nav_ul").hover(
+		function() {
+			$(".nav").css("z-index", "50");
+		},
+		function() {
+			$(".nav").css("z-index", "-2");
+		}
+	);
+
+	$(".nav_ul li").click(function() {
 		var tag_id = $(this).parent().prev().attr("id");
 		$("#blog_background").attr("src", "../static/img/" + tag_id + "_content.png");
+		$(".tag_blogs").css("visibility", "hidden");
+		$("#" + tag_id + "_blog").css("visibility", "visible");
 	});
-});
+
+	$(".nav_ul li").hover(
+		function() {
+			$(this).css("color", "#fff");
+		},
+		function() {
+			$(this).css("color", "#000");
+		}
+	);
+
+});	
